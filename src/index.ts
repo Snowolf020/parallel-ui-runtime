098b371f34fadb61b9ac97327a72a8a25fa683bc
Snowolf020 <Snowolf020@users.noreply.github.com>
2026-06-14 03:55:42 +0000

    init project with vite and react

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import React from 'react';
+import { createApp } from 'vite';
+import { render } from 'react-dom';
+import App from './App';
+
+const app = createApp();
+render(<App />, document.getElementById('root'));
